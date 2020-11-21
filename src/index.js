b5eb669dd951e24d117b9b829637b1ac6c28d4be
Jan Schill <janschill@me.com>
2020-11-21 13:48:54 +0100

    Include Solid-Auth-Client

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
+import SolidAuthClient from 'solid-auth-client';
 import DomNodeCreator from './dom-node-creator.js';
-new DomNodeCreator().createCommentModule()
+
+const domNodeCreator = new DomNodeCreator()
+domNodeCreator.createCommentModule()
+
+async function popupLogin() {
+  let session = await SolidAuthClient.currentSession();
+  let popupUri = 'https://solidcommunity.net/common/popup.html';
+  if (!session)
+    session = await SolidAuthClient.popupLogin({ popupUri });
+  alert(`Logged in as ${session.webId}`);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  domNodeCreator.$commentNode.$login.$node.addEventListener('click', () => {
+    popupLogin();
+  })
+})
