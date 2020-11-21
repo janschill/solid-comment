7428c447cd3448aa36d168a6d2aca37bad7110a0
Jan Schill <janschill@me.com>
2020-11-21 23:22:20 +0100

    Remove class from app.js

--- src/app.js
+++ src/app.js
@@ -2,34 +2,27 @@ import SolidAuthClient from 'solid-auth-client';
 import DomNodeCreator from './dom-node-creator.js';
 import * as Util from './util.js'
 
-class App {
-  static async main() {
-    this.domNodeCreator = new DomNodeCreator();
-    this.domNodeCreator.createCommentModule();
-    App.attachSolidLoginToSolidCommentButton();
-    this.session = await SolidAuthClient.currentSession();
-    console.log(this.session);
-  }
+const domNodeCreator = new DomNodeCreator();
+domNodeCreator.createCommentModule();
+attachSolidLoginToSolidCommentButton();
 
-  static attachSolidLoginToSolidCommentButton() {
-    async function popupLogin() {
-      let popupUri = 'https://solidcommunity.net/common/popup.html';
-      if (!this.session) {
-        this.session = await SolidAuthClient.popupLogin({ popupUri });
-        console.log(this.session.webId);
-      }
-      alert(`Logged in as ${this.session.webId}`);
+async function attachSolidLoginToSolidCommentButton() {
+  async function popupLogin() {
+    const session = await SolidAuthClient.currentSession();
+    let popupUri = 'https://solidcommunity.net/common/popup.html';
+    if (!session) {
+      session = await SolidAuthClient.popupLogin({ popupUri });
+      console.log(session.webId);
     }
-
-    Util.executeOnDOMReady(() => {
-      this.domNodeCreator.$commentNode.$loginButton.$node.addEventListener('click', () => {
-        popupLogin();
-      });
-      this.domNodeCreator.$commentNode.$logoutButton.$node.addEventListener('click', () => {
-        SolidAuthClient.logout();
-      });
-    });
+    alert(`Logged in as ${session.webId}`);
   }
-}
 
-App.main()
+  Util.executeOnDOMReady(() => {
+    domNodeCreator.$commentNode.$loginButton.$node.addEventListener('click', () => {
+      popupLogin();
+    });
+    domNodeCreator.$commentNode.$logoutButton.$node.addEventListener('click', () => {
+      SolidAuthClient.logout();
+    });
+  });
+}
