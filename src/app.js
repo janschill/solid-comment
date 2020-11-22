759f649799a2e0383f5e44878f79ec42431cc5f7
Jan Schill <janschill@me.com>
2020-11-22 21:04:12 +0100

    Cleanup entry

--- src/app.js
+++ src/app.js
@@ -1,47 +1,33 @@
 import store from './store/index.js';
 import SolidAuthClient from 'solid-auth-client';
-import * as Util from './util.js'
 import './styles.css'
 
-// Import components
 import Session from './components/session.js';
-import Comment from './components/comment.js';
+import LoginButton from './components/login-button.js';
+import LogoutButton from './components/logout-button.js';
 
-// Instantiate components
-const session = new Session();
-const comment = new Comment();
-
-// Render components
-session.render();
-comment.render();
-
-SolidAuthClient.trackSession(solidSession => {
-  if (solidSession)
-    store.dispatch('setSession', solidSession.webId);
-});
-
-async function attachSolidLoginToSolidCommentButton() {
-  async function popupLogin() {
-    let solidSession = await SolidAuthClient.currentSession();
-    let popupUri = 'https://solidcommunity.net/common/popup.html';
-    if (!solidSession) {
-      solidSession = await SolidAuthClient.popupLogin({ popupUri });
+async function checkSession() {
+  SolidAuthClient.trackSession(solidSession => {
+    if (solidSession) {
       store.dispatch('setSession', solidSession.webId);
-      console.log(solidSession.webId);
     }
-    alert(`Logged in as ${solidSession.webId}`);
-  }
-
-  Util.executeOnDOMReady(() => {
-    const $loginButton = comment.element.querySelector('.solid-comment-button--login');
-    $loginButton.addEventListener('click', () => {
-      popupLogin();
-    });
-    const $logoutButton = comment.element.querySelector('.solid-comment-button--logout');
-    $logoutButton.addEventListener('click', () => {
-      SolidAuthClient.logout();
-    });
   });
 }
 
-attachSolidLoginToSolidCommentButton();
+function create() {
+  return {
+    session: new Session(),
+    loginButton: new LoginButton(),
+    logoutButton: new LogoutButton(),
+  }
+}
+
+function render(components) {
+  components.session.render();
+  components.loginButton.render();
+  components.logoutButton.render();
+}
+
+const components = create();
+render(components);
+checkSession()
