307786b9b6afa4b36bec81d8c71fcef05e0eae83
Jan Schill <janschill@me.com>
2020-11-22 14:17:16 +0100

    WIP Solid auth

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import store from './store/index.js';
+import SolidAuthClient from 'solid-auth-client';
+import * as Util from './util.js'
 
 // Import components
 import Session from './components/session.js';
@@ -11,3 +13,34 @@ const comment = new Comment();
 // Render components
 session.render();
 comment.render();
+
+SolidAuthClient.trackSession(solidSession => {
+  if (solidSession)
+    store.dispatch('setSession', solidSession.webId);
+});
+
+async function attachSolidLoginToSolidCommentButton() {
+  async function popupLogin() {
+    let solidSession = await SolidAuthClient.currentSession();
+    let popupUri = 'https://solidcommunity.net/common/popup.html';
+    if (!solidSession) {
+      solidSession = await SolidAuthClient.popupLogin({ popupUri });
+      store.dispatch('setSession', solidSession.webId);
+      console.log(solidSession.webId);
+    }
+    alert(`Logged in as ${solidSession.webId}`);
+  }
+
+  Util.executeOnDOMReady(() => {
+    const $loginButton = comment.element.querySelector('.solid-comment-button--login');
+    $loginButton.addEventListener('click', () => {
+      popupLogin();
+    });
+    const $logoutButton = comment.element.querySelector('.solid-comment-button--logout');
+    $logoutButton.addEventListener('click', () => {
+      SolidAuthClient.logout();
+    });
+  });
+}
+
+attachSolidLoginToSolidCommentButton();
