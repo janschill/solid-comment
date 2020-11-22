79b4486b1bc086f0a876c9cdeb5c1e138187a27c
Jan Schill <janschill@me.com>
2020-11-22 17:41:04 +0100

    Configure Tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,26 @@
 module.exports = {
   entry: './src/app.js',
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      }
+    ]
+  }
 };
