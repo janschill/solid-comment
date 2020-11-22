75f70cd8e998f849dbae93ffa1cded15e33ca00f
Jan Schill <janschill@me.com>
2020-11-22 13:43:03 +0100

    WIP render comment module

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ import store from './store/index.js';
 
 // Import components
 import Session from './components/session.js';
+import Comment from './components/comment.js';
 
 // Instantiate components
 const session = new Session();
+const comment = new Comment();
 
 // Render components
 session.render();
+comment.render();
---
+++ src/components/comment.js
@@ -0,0 +1,17 @@
+import store from '../store/index.js';
+import Component from '../../lib/component.js';
+import CommentNode from '../comment-node.js';
+
+export default class Comment extends Component {
+  constructor() {
+    super({
+      store,
+      element: document.querySelector('.solid-comment-parent-node')
+    });
+  }
+
+  render() {
+    const commentNode = new CommentNode();
+    this.element = this.element.appendChild(commentNode.$node);
+  }
+}
