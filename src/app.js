766563c42a068dfa35fe2ad592881543ce4a8a92
Jan Schill <janschill@me.com>
2020-11-28 00:25:54 +0100

    Clean up app class

--- src/app.js
+++ src/app.js
@@ -15,29 +15,18 @@ class App {
     //   show login
     //   no comments here
 
-    const session = await this.checkSession();
-    if (session) {
-      Comment.all()
-      this.loadComments();
-    }
   }
 
-  async loadComments() {
-    const solidClient = new SolidClient();
-    const commentContainer = '/solid-comment/';
-    const commentFileValue = await solidClient.get(`${commentContainer}text.text`);
-    store.dispatch('setComments', await commentFileValue.text());
-  }
+  async load() {
+    // const client = new SolidClient();
+    // const session = client.session();
 
-  async checkSession() {
-    trackSession(session => {
-      if (session) {
-        store.dispatch('setSession', session.webId);
-        return session;
-      }
-      return store.state.session;
-    });
+    // if (session) {
+    Comment.all()
+    // this.loadComments();
+    // }
   }
 }
 
-new App();
+const app = new App();
+app.load();
