21f5a2ed0e922982e948deecef033bcd36fec795
Jan Schill <janschill@me.com>
2020-11-30 11:03:57 +0100

    Use code splitting in Webpack
    * Use two different entry points
    * Load external dependencies separately
    
    Going from 15s to 80ms per compile

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
+const path = require('path');
+
 module.exports = {
-  entry: './src/app.js',
   // devtool: 'inline-source-map',
+  mode: 'development',
+  entry: {
+    styles: './src/styles.js',
+    app: './src/app.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
   resolve: {
     fallback: { "buffer": require.resolve("buffer/") },
   },
@@ -10,20 +23,21 @@ module.exports = {
         test: /\.css$/,
         exclude: /node_modules/,
         use: [
-          {
-            loader: 'style-loader',
-          },
+          { loader: 'style-loader' },
           {
             loader: 'css-loader',
             options: {
               importLoaders: 1,
             }
           },
-          {
-            loader: 'postcss-loader'
-          }
+          { loader: 'postcss-loader' }
         ]
       }
     ]
-  }
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
 };
