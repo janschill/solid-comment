796b29db88490f2b4757150c2c90bd1f18d4bf03
Jan Schill <janschill@me.com>
2020-11-30 14:20:13 +0100

    Remove comments

--- src/app.js
+++ src/app.js
@@ -4,18 +4,10 @@ import Comment from './models/comment.js';
 class App {
   constructor() {
     new Home().instantiateComponents();
-    // if session
-    //   connect to pod
-    //   load comments
-    // else
-    //   show login
-    //   no comments here
   }
 
   async load() {
     Comment.all()
-    // const client = new SolidClient();
-    // const session = client.session();
   }
 }
 
