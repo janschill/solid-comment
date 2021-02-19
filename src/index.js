4b0e3f993c9479aa1669bfc70bd4e8ee862d266b
Jan Schill <janschill@me.com>
2021-02-19 17:18:58 +0100

    WIP reading data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+import { Comment } from "./models/comment";
 import { SolidClient } from "./solid/solid-client";
+import {
+  getSolidDataset,
+  getThing,
+  getStringNoLocale
+} from "@inrupt/solid-client";
+import { VCARD } from "@inrupt/vocab-common-rdf";
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log('DOM ready');
@@ -19,3 +26,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
   main()
 });
+
+export class SolidComment {
+  constructor(configuration) {
+    this.solidCommentId = configuration.solidCommentId;
+    this.serverStorageEndpoint = configuration.serverStorageEndpoint;
+    this.webIdsOfAuthors = configuration.webIdsOfAuthors;
+
+    this.main();
+  }
+
+  async readComments() {
+    const resourceUrl = `https://janschill.net/solid-comment/${this.solidCommentId}/example.ttl`;
+    const dataset = await getSolidDataset(resourceUrl);
+    const resource = getThing(dataset, resourceUrl);
+    // const fn = getStringNoLocale(profile, VCARD.fn);
+    // const role = getStringNoLocale(profile, VCARD.role);
+    return resource
+  }
+
+  async main() {
+    const comments = await this.readComments()
+    console.log(comments)
+
+
+    // new Comment({
+    //   author: "https://janschill.net/profile/card#me",
+    //   text: "My first comment",
+    //   date: new Date()
+    // })
+    // fetch comments
+    // render elements
+    //
+  }
+}
+
+window.SolidComment = SolidComment;
