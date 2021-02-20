26d2a75ff4cc7baa14bcb61075c55cb6292ba94d
Jan Schill <janschill@me.com>
2021-02-20 01:15:57 +0100

    Clean up index

--- src/index.js
+++ src/index.js
@@ -1,51 +1,39 @@
-import { Comment } from "./models/comment";
 import { SolidClient } from "./solid/solid-client";
-import {
-  getSolidDataset,
-  getThing,
-  getStringNoLocale,
-  getUrl
-} from "@inrupt/solid-client";
-import { SCHEMA_INRUPT_EXT, VCARD } from "@inrupt/vocab-common-rdf";
-import {
-  handleIncomingRedirect,
-} from "@inrupt/solid-client-authn-browser"
-import { LoginButton } from "./components/login-button";
-
 import Home from "./home";
-import store from "./store";
 
 class App {
   constructor() {
     new Home().instantiateComponents();
-    store.dispatch("setSession", { webId: "Jan" })
+  }
+
+  async boot() {
+    this.solidClient = new SolidClient()
+    this.solidClient.login("https://janschill.net")
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM ready");
-  const app = new App();
-
   async function main() {
-    await handleIncomingRedirect();
-    const solidClient = new SolidClient();
+    console.log("DOM ready");
+    const app = new App();
+    app.boot()
 
-    const $solidOidcIssuer = document.querySelector("#solid-oidc-issuer");
-    const $solidLoginButton = document.querySelector("#solid-login-button");
-    if ($solidLoginButton && $solidOidcIssuer) {
-      $solidLoginButton.addEventListener("click", async () => {
-        const solidOidcIssuer = $solidOidcIssuer.value;
-        if (solidOidcIssuer) {
-          await solidClient.login(solidOidcIssuer)
-          console.log(solidClient.session)
-        }
-      });
-    }
+    // const $solidOidcIssuer = document.querySelector("#solid-oidc-issuer");
+    // const $solidLoginButton = document.querySelector("#solid-login-button");
+    // if ($solidLoginButton && $solidOidcIssuer) {
+    //   $solidLoginButton.addEventListener("click", async () => {
+    //     const solidOidcIssuer = $solidOidcIssuer.value;
+    //     if (solidOidcIssuer) {
+    //       await solidClient.login(solidOidcIssuer)
+    //       console.log(solidClient.session)
+    //     }
+    //   });
+    // }
 
-    console.log("isLoggedIn", solidClient.isLoggedIn());
+    // console.log("isLoggedIn", solidClient.isLoggedIn());
   }
 
-  // main()
+  main()
 });
 
 export class SolidComment {
@@ -53,39 +41,6 @@ export class SolidComment {
     this.solidCommentId = configuration.solidCommentId;
     this.serverStorageEndpoint = configuration.serverStorageEndpoint;
     this.webIdsOfAuthors = configuration.webIdsOfAuthors;
-
-    this.main();
-  }
-
-  async readComments() {
-    const resourceUrl = `https://janschill.net/solid-comment/${this.solidCommentId}/comments.ttl`;
-    const dataset = await getSolidDataset(resourceUrl);
-    const resource = getThing(dataset, `${resourceUrl}#it`);
-    const author = getUrl(resource, SCHEMA_INRUPT_EXT.NS("creator"));
-    const text = getStringNoLocale(resource, SCHEMA_INRUPT_EXT.NS("commentText"));
-    const time = getStringNoLocale(resource, SCHEMA_INRUPT_EXT.NS("commentTime"));
-    console.log("author", author)
-    console.log("text", text)
-    console.log("time", time)
-    return resource
-  }
-
-  async writeComments() {
-
-  }
-
-  async main() {
-    // const comments = await this.readComments()
-
-
-    // new Comment({
-    //   author: "https://janschill.net/profile/card#me",
-    //   text: "My first comment",
-    //   date: new Date()
-    // })
-    // fetch comments
-    // render elements
-    //
   }
 }
 
