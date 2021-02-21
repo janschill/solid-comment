768fc931167491555ea4949afb5335010d19aa74
Jan Schill <janschill@me.com>
2021-02-21 23:17:45 +0100

    Introduce agent class

---
+++ src/models/solid-agent.js
@@ -0,0 +1,27 @@
+import {
+  getSolidDataset,
+  getStringNoLocale,
+  getThing,
+  getUrl,
+} from "@inrupt/solid-client"
+import { VCARD } from "@inrupt/vocab-common-rdf";
+
+export default class SolidAgent {
+  constructor() {}
+
+  async fetchProfile(webIdUrl) {
+    const profileDataset = await getSolidDataset(webIdUrl);
+    const profile = getThing(
+      profileDataset,
+      webIdUrl
+    );
+    this.fullName = getStringNoLocale(profile, VCARD.fn);
+    this.photo = getUrl(profile, VCARD.hasPhoto);
+    this.organizationName = getStringNoLocale(profile, VCARD.organization_name);
+    this.role = getStringNoLocale(profile, VCARD.role);
+  }
+
+  initials() {
+    return this.fullName.split(" ").reduce((acc, el) => acc + el[0], "");
+  }
+}
