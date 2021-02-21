5f6d4da798479242c1f098541488cc3f4b9a22c8
Jan Schill <janschill@me.com>
2021-02-21 21:18:43 +0100

    Add comments

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,11 @@
 export default {
+  pushComment(context, payload) {
+    context.commit("pushComment", payload);
+  },
+  setComments(context, payload) {
+    context.commit("setComments", payload);
+  },
   setSession(context, payload) {
-    context.commit('setSession', payload)
+    context.commit("setSession", payload);
   },
 }
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,4 +1,14 @@
 export default {
+  addComment(state, payload) {
+    state.comments.push(payload);
+
+    return state;
+  },
+  setComments(state, payload) {
+    state.comments = payload;
+
+    return state;
+  },
   setSession(state, payload) {
     state.session = payload;
 
