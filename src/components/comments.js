90e6faf5b1bc5a2a007d876fc5e1db32d4299b06
Jan Schill <janschill@me.com>
2021-02-23 01:50:28 +0100

    Apply eslint

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,32 +1,32 @@
-import Component from "./component";
-import store from "../store";
-import Time from "../util/time";
+import Component from './component'
+import store from '../store'
+import Time from '../util/time'
 
 export default class Comments extends Component {
-  constructor() {
+  constructor () {
     super({
       store,
-      element: document.querySelector(".sc-list")
-    });
+      element: document.querySelector('.sc-list')
+    })
   }
 
-  async render() {
-    let html = "";
+  async render () {
+    let html = ''
 
     store.state.comments.forEach(comment => {
       html += `
       <li class="sc-list__item">
         <article class="sc-list__article">
           <header class="sc-list__header">
-            <abbr class="sc-list__date" title="${comment.time}">${Time.format("M d", comment.time)}</abbr>
+            <abbr class="sc-list__date" title="${comment.time}">${Time.format('M d', comment.time)}</abbr>
             <address class="sc-list__author"><abbr title="${comment.author.fullName}">${comment.author.initials()}</abbr></address>
           </header>
           <p class="sc-list__text">${comment.text}</p>
         </article>
       </li>
       `
-    });
+    })
 
-    this.element.innerHTML = html;
+    this.element.innerHTML = html
   }
 }
