57bb423d40767b9c3bc826db5c1d49f865d2cdf0
Jan Schill <janschill@me.com>
2021-02-23 02:33:22 +0100

    Update markup

--- src/components/comments.js
+++ src/components/comments.js
@@ -17,11 +17,18 @@ export default class Comments extends Component {
       html += `
       <li class="sc-list__item">
         <article class="sc-list__article">
-          <header class="sc-list__header">
-            <abbr class="sc-list__date" title="${comment.time}">${Time.format('M d', comment.time)}</abbr>
-            <address class="sc-list__author"><abbr title="${comment.author.fullName}">${comment.author.initials()}</abbr></address>
-          </header>
-          <p class="sc-list__text">${comment.text}</p>
+          <aside class="sc-list__aside">
+            <a href="${comment.author.webIdUrl}" target="_blank">
+              <img class="sc-list__image" src="${comment.author.photo}" alt="${comment.author.fullName}">
+            </a>
+          </aside>
+          <section class="">
+            <header class="sc-list__header">
+              <address class="sc-list__author"><a href="${comment.author.webIdUrl}" target="_blank">${comment.author.fullName}</a></address> ·
+              <abbr class="sc-list__date" title="${comment.time}">${Time.format('M d', comment.time)}</abbr>
+            </header>
+            <p class="sc-list__text">${comment.text}</p>
+          </section>
         </article>
       </li>
       `
