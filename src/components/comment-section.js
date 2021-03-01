3d0fea32ff255d6dfad6fb9334d255938ff204f7
Jan Schill <janschill@me.com>
2021-03-01 01:01:59 +0100

    WIP weird render bug

--- src/components/comment-section.js
+++ src/components/comment-section.js
@@ -14,9 +14,10 @@ export default class CommentSection extends Component {
   render () {
     const isLoggedIn = get(store, 'state.session.data.session.info.isLoggedIn')
 
+    // TODO: This is for some reason causing issues all of a sudden
     if (isLoggedIn === undefined || !isLoggedIn) {
-      this.element.style.display = 'block'
-      this.element.innerHTML = '<p>You need to be logged in with your WebID to submit a comment.'
+      // this.element.style.display = 'block'
+      // this.element.innerHTML = '<p>You need to be logged in with your WebID to submit a comment.'
     }
   }
 }
