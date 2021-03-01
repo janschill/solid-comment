f505f64c9168171e2bdbe1d4f388d57defba096c
Jan Schill <janschill@me.com>
2021-03-01 01:01:21 +0100

    Don't load when none are there

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import SolidClient from './auth/solid-client'
 import Home from './home'
-import Comment from './models/comment'
 
 export default class App {
   constructor () {
@@ -11,6 +10,5 @@ export default class App {
   async boot () {
     this.solidClient = new SolidClient()
     await this.solidClient.checkSession()
-    await Comment.all()
   }
 }
