d3f58bb7332e802d1008355b35a9c930da5037a4
Jan Schill <janschill@me.com>
2021-03-19 22:03:46 +0100

    Disable hint

--- src/components/comment-section.js
+++ src/components/comment-section.js
@@ -12,12 +12,12 @@ export default class CommentSection extends Component {
   }
 
   render () {
-    const isLoggedIn = get(store, 'state.session.data.session.info.isLoggedIn')
+    // const isLoggedIn = get(store, 'state.session.data.session.info.isLoggedIn')
 
     // TODO: This is for some reason causing issues all of a sudden
-    if (isUndefined(isLoggedIn) || !isLoggedIn) {
-      this.element.style.display = 'block'
-      this.element.innerHTML = '<p>You need to be logged in with your WebID to submit a comment.</p>'
-    }
+    // if (isUndefined(isLoggedIn) || !isLoggedIn) {
+    // this.element.style.display = 'block'
+    // this.element.innerHTML = '<p>You need to be logged in with your WebID to submit a comment.</p>'
+    // }
   }
 }
