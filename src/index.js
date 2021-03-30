7ab453d73775990c0fc70333b6d55818a4c5d486
Jan Schill <janschill@me.com>
2021-03-30 14:52:03 +0200

    Console log the package version

--- src/index.js
+++ src/index.js
@@ -6,13 +6,11 @@ import './assets/css/indico.css'
 import { addToConfig, addObjectToConfig } from './config'
 import App from './app'
 import Comment from './models/comment'
+const packageJson = require('../package.json')
 
 export class SolidComment {
   constructor (configuration) {
-    console.log('Solid Comment loaded')
-    addObjectToConfig(configuration)
-    addToConfig('appName', 'Solid-Comment')
-    addToConfig('resourceContainerPath', `solid-comment/${configuration.solidCommentId}`)
+    console.log(`Solid Comment ${packageJson.version} loaded.`)
     const endpoint = configuration.serverStorageEndpointUrl
     if (!isString(endpoint) && !isNil(endpoint) && !isUndefined(endpoint)) {
       throw Error('Please specify a valid storage endpoint, that can receive JSON POST requests.')
