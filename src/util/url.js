ec29c7d1630c2d4fa81f9e412e42ee963ea697a9
Jan Schill <janschill@me.com>
2021-04-06 19:45:53 +0200

    Introduce URL helper functions

--- src/util/url.js
+++ src/util/url.js
@@ -1,3 +1,22 @@
 export function originFromUrl (url) {
   if (typeof url === 'string' && url.includes('http')) { return new URL(url).origin }
 }
+
+export function hasHttp (urlString) {
+  return urlString.search(/^http:\/\//) === 0
+}
+
+export function hasHttps (urlString) {
+  return urlString.search(/^https:\/\//) === 0
+}
+
+export function setHttps (urlString) {
+  if (urlString !== '' && !hasHttps(urlString)) {
+    if (hasHttp(urlString)) {
+      urlString = urlString.replace('http', 'https')
+    } else {
+      urlString = `https://${urlString}`
+    }
+  }
+  return urlString
+}
